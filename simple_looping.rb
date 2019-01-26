fcf528f983af79dba958ebebb80ca9ebf1ac726e
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-26 21:11:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,12 +1,12 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times = 5)
-  #loop do 
-   # number_of_times += 1
-    #puts phrase
-    #if number_of_times >= 5
-    #  break
-    #end
+def loop_iterator(number_of_times = 7)
+  loop do 
+    number_of_times += 1
+    puts phrase
+    if number_of_times >= 5
+      break
+    end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
